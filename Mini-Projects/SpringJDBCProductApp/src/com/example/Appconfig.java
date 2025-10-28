c50916a5542be3dc9072c8193fef0fe302a43d79
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-10-28 22:44:36 +0530

    Update Appconfig.java

--- Mini-Projects/SpringJDBCProductApp/src/com/example/Appconfig.java
+++ Mini-Projects/SpringJDBCProductApp/src/com/example/Appconfig.java
@@ -1,1 +1,35 @@
+package com.example.product.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.example.product.dao.ProductDAOImpl;
+
+@Configuration
+@ComponentScan("com.example.product")
+public class AppConfig {
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/spring_productdb");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public ProductDAOImpl productDAO() {
+        ProductDAOImpl dao = new ProductDAOImpl();
+        dao.setJdbcTemplate(jdbcTemplate());
+        return dao;
+    }
+}
 
