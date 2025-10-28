494a25d49bddeb3276adc6a8f1b67147f00d6b67
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-10-28 22:43:47 +0530

    Define ProductDAO interface for product operations

--- Mini-Projects/SpringJDBCProductApp/src/com/example/ProductDAO.java
+++ Mini-Projects/SpringJDBCProductApp/src/com/example/ProductDAO.java
@@ -1,1 +1,13 @@
+package com.example.product.dao;
+
+import java.util.List;
+import com.example.product.model.Product;
+
+public interface ProductDAO {
+    int addProduct(Product product);
+    int updateProduct(Product product);
+    int deleteProduct(int id);
+    Product getProductById(int id);
+    List<Product> getAllProducts();
+}
 
