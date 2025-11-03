a9596be3cddf880a9a75115039a03b52c249344f
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-11-03 23:32:38 +0530

    Update course.java

--- Mini-Projects/Hibernate-Student-Course/src/main/java/com/example/hibernate/course.java
+++ Mini-Projects/Hibernate-Student-Course/src/main/java/com/example/hibernate/course.java
@@ -1,1 +1,41 @@
 
+package com.project;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String instructor;
+
+    @ManyToMany(mappedBy = "courses")
+    private Set<Student> students = new HashSet<>();
+
+    public Course() {}
+
+    public Course(String title, String instructor) {
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getInstructor() { return instructor; }
+    public void setInstructor(String instructor) { this.instructor = instructor; }
+
+    public Set<Student> getStudents() { return students; }
+    public void setStudents(Set<Student> students) { this.students = students; }
+}
