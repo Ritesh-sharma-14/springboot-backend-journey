cf2c874fc4eaefb21b926445ad5cbd4df10091c4
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-11-10 22:03:10 +0530

    Update AppConfig.java

--- Mini-Projects/SpringCore/com.SpringCoredemo/Config/AppConfig.java
+++ Mini-Projects/SpringCore/com.SpringCoredemo/Config/AppConfig.java
@@ -1,1 +1,9 @@
+package com.springcoredemo.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.springdemo")
+  public class AppConfig{
+  }
