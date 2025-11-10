ebd5d74d0a24b888471854ae026a9af3211dfa13
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-11-10 21:50:31 +0530

    Update EmailSevices.java

--- Mini-Projects/SpringCore/com.SpringCoredemo/services/EmailSevices.java
+++ Mini-Projects/SpringCore/com.SpringCoredemo/services/EmailSevices.java
@@ -1,1 +1,10 @@
+package com.springcoredemo.service;
 
+import org.springframework.stereotype.Component;
+
+@Component //Marks this as a springBean
+public class EmailService implements MessageService{
+  public void sendMessage(String message){
+     System.out.println("Sending Email: " +message);
+  }
+}
