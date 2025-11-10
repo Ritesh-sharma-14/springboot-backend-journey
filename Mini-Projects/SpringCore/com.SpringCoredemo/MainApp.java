4e327ca73566773825e9065cb3fb21520710a63a
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-11-10 22:10:08 +0530

    Update MainApp.java

--- Mini-Projects/SpringCore/com.SpringCoredemo/MainApp.java
+++ Mini-Projects/SpringCore/com.SpringCoredemo/MainApp.java
@@ -1,1 +1,15 @@
+package com.springcoredemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.springcoredemo.config.AppConfig;
+import com.springcoredemo.service.MessageService;
+
+Public class MainApp{
+  public static void main(String[] args){
+     ApplicationContext context = new AnnotationConfig ApplicationContext(App.class);
+     MessageService messageServices = context.getBean(MessageServices.class);
+     messageServices.sendMessage("Hello from Spring Core!");
+  }
+}
 
