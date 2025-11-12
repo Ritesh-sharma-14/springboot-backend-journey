bd5dfb0bc79598c0ea0a40555e374c2d79f42830
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-11-12 23:44:16 +0530

    Create BlogController.java

---
+++ Mini-Projects/BlogApp/src/main/java/com/example/blogApp/controller/BlogController.java
@@ -0,0 +1,45 @@
+package com.example.blogapp.controller;
+
+import com.example.blogapp.entity.Blog;
+import com.example.blogapp.service.BlogService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/blogs")
+public class BlogController {
+
+    private final BlogService blogService;
+
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
+    @GetMapping
+    public List<Blog> getAllBlogs() {
+        return blogService.getAllBlogs();
+    }
+
+    @GetMapping("/{id}")
+    public Blog getBlogById(@PathVariable Long id) {
+        return blogService.getBlogById(id)
+                .orElseThrow(() -> new RuntimeException("Blog not found"));
+    }
+
+    @PostMapping
+    public Blog createBlog(@RequestBody Blog blog) {
+        return blogService.createBlog(blog);
+    }
+
+    @PutMapping("/{id}")
+    public Blog updateBlog(@PathVariable Long id, @RequestBody Blog blog) {
+        return blogService.updateBlog(id, blog);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBlog(@PathVariable Long id) {
+        blogService.deleteBlog(id);
+        return "Blog deleted successfully";
+    }
+}
